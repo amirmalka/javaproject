ee496d6b611a48fd226f94e0c1aaff98ca3ed615
Yagil <ran.yagil@hpe.com>
2017-08-13 23:19:59 +0300

    Update Unit Test

--- src/main/test/com/hit/algorithm/IAlgoCacheTest.java
+++ src/main/test/com/hit/algorithm/IAlgoCacheTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IAlgoCacheTest{
+	
 	/*
 	 * This unit test will check the following regarding each cache algorithm:
 	 * Get an existing key - its value needs to be returned
@@ -15,12 +16,13 @@ public class IAlgoCacheTest{
 	 * Remove an existing element from the cache - currentSame needs to be decremented by 1 and the key must be removed from the cache
 	 * Check time complexity of each algorithm (necessary?)
 	 */
+	
 	@Test
 	public void testLRU() {
-		int casheSize = 4;
-		LRUAlgoCacheImpl<Integer,Integer> testCache = new LRUAlgoCacheImpl<Integer,Integer>(casheSize);
-		for(int i=1;i<casheSize+1;i++) {
-			testCache.putElement(i, i+casheSize);
+		int cacheSize = 4;
+		LRUAlgoCacheImpl<Integer,Integer> testCache = new LRUAlgoCacheImpl<Integer,Integer>(cacheSize);
+		for(int i=1;i<cacheSize+1;i++) {
+			testCache.putElement(i, i*100);
 		}
 		
 		//Check LRU Total Capacity:
@@ -36,25 +38,21 @@ public class IAlgoCacheTest{
 		 assertEquals(3, testCache.getCacheCurrentSize());
 		 
 		//Put  element #5 in the cache
-		 testCache.putElement(5, 5+casheSize);
+		 testCache.putElement(5, 5*100);
 		 
 		//Check LRU Current Capacity:
 		 assertEquals(4, testCache.getCacheCurrentSize());
 		 
 		//Current State in cache: LRU:#1, MRU:#5
 		//Put an additional element in the cache- causes a page fault:
-		 testCache.putElement(6, 6+casheSize);
-		 //element #1 needs to be paged:
-		 assertEquals(null, testCache.getElement(1));
-		 assertEquals(null, testCache.getElement(4));
-		 assertEquals(Integer.valueOf(6+casheSize), testCache.getElement(6)); 
-		 assertEquals(Integer.valueOf(5+casheSize), testCache.getElement(5));
-		 assertEquals(Integer.valueOf(3+casheSize), testCache.getElement(3));
-		 assertEquals(Integer.valueOf(2+casheSize), testCache.getElement(2));
+		 assertEquals(Integer.valueOf(100),testCache.putElement(6, 6*100));
+		 assertEquals(Integer.valueOf(6*100), testCache.getElement(6)); 
+		 assertEquals(Integer.valueOf(5*100), testCache.getElement(5));
+		 assertEquals(Integer.valueOf(3*100), testCache.getElement(3));
+		 assertEquals(Integer.valueOf(2*100), testCache.getElement(2));
 		//Put an additional element in the cache- causes a page fault:
-		 testCache.putElement(7, 7+casheSize);
-		 assertEquals(null, testCache.getElement(6));
-		 assertEquals(Integer.valueOf(7+casheSize), testCache.getElement(7));
+		 assertEquals(Integer.valueOf(600),testCache.putElement(7, 7*100));
+		 assertNull(testCache.getElement(6));
 		 
 		 //Remove all element from the cache
 		 testCache.removeElement(5);
@@ -67,12 +65,13 @@ public class IAlgoCacheTest{
 		 assertEquals(0, testCache.getCacheCurrentSize());
 	}
 	
+	
 	@Test 
 	public void testRandom() {
-		int casheSize = 4;
-		Random<Integer,Integer> testCache = new Random<Integer,Integer>(casheSize);
-		for(int i=1;i<casheSize+1;i++) {
-			testCache.putElement(i, i+casheSize);
+		int cacheSize = 4;
+		Random<Integer,Integer> testCache = new Random<Integer,Integer>(cacheSize);
+		for(int i=1;i<cacheSize+1;i++) {
+			testCache.putElement(i, i+cacheSize);
 		}
 		
 		//Check LRU Total Capacity:
@@ -88,25 +87,18 @@ public class IAlgoCacheTest{
 		 assertEquals(3, testCache.getCacheCurrentSize());
 		 
 		//Put element #5 in the cache
-		 testCache.putElement(5, 5+casheSize);
+		 testCache.putElement(5, 5+cacheSize);
 		 
 		//Check LRU Current Capacity:
 		 assertEquals(4, testCache.getCacheCurrentSize());
 		 
 		//Put an additional element in the cache- causes a page fault:
-		 testCache.putElement(6, 6+casheSize);
+		 testCache.putElement(6, 6+cacheSize);
 		 assertEquals(4, testCache.getCacheCurrentSize());
 		 
 		 //Remove all elements from the cache
-		 if(testCache.getElement(1)!=null)
-			 testCache.removeElement(1);
-		 if (testCache.getElement(2)!=null)
-			 testCache.removeElement(2);
-		 if (testCache.getElement(3)!=null)
-			 testCache.removeElement(3);
-		 if (testCache.getElement(5)!=null)
-			 testCache.removeElement(5);
-		 testCache.removeElement(6);
+		 for(int i=0;i<7;i++)
+			 testCache.removeElement(i);
 		 
 		 assertEquals(0, testCache.getCacheCurrentSize());
 	}
