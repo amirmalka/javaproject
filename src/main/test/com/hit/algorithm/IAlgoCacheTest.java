3f4b44a4461e5246206ae8e81ccff15003089f59
Yagil <ran.yagil@hpe.com>
2017-08-13 23:21:33 +0300

    Delete comments

--- src/main/test/com/hit/algorithm/IAlgoCacheTest.java
+++ src/main/test/com/hit/algorithm/IAlgoCacheTest.java
@@ -6,17 +6,6 @@ import org.junit.Test;
 
 public class IAlgoCacheTest{
 	
-	/*
-	 * This unit test will check the following regarding each cache algorithm:
-	 * Get an existing key - its value needs to be returned
-	 * Get an non-existing key - null value needs to be returned
-	 * Put an element to a non-full cache - verify cache's values.
-	 * Put an element to a full cache - a page fault needs to be performed - check the key which is replaced depends on the Algorithm.
-	 * Remove a non-existing element from the cache - nothing happens and the currentSame should stay the same, the keys stay the same in the cache
-	 * Remove an existing element from the cache - currentSame needs to be decremented by 1 and the key must be removed from the cache
-	 * Check time complexity of each algorithm (necessary?)
-	 */
-	
 	@Test
 	public void testLRU() {
 		int cacheSize = 4;
