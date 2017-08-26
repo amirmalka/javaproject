b58f4d913f94414e005d39a5b9a9ff3615a21cb7
Yagil <ran.yagil@hpe.com>
2017-08-26 18:18:00 +0300

    MMU

--- MMUProject/src/main/java/com/hit/memoryunits/MemoryManagementUnit.java
+++ MMUProject/src/main/java/com/hit/memoryunits/MemoryManagementUnit.java
@@ -1,7 +1,18 @@
 package com.hit.memoryunits;
+import com.hit.algorithm.*;
 
 public class MemoryManagementUnit {
+	 private RAM myRAM;
+	 private IAlgoCache<Long,Long> myAlgoCache;
+	 
+	MemoryManagementUnit(int ramCapacity, IAlgoCache<Long,Long> algo) {
+		this.myRAM = new RAM(ramCapacity);
+		this.myAlgoCache = algo ;
+	}
 	
+	public Page<byte[]>[] getPages(Long[] pageIds) throws java.io.IOException{
+		
+	}
 }
 
 
