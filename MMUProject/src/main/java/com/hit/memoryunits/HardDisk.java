ed780136aa95169dd50c0900df8b7f2780c5a18a
Malka <Amir.Malka@emc.com>
2017-08-26 18:16:48 +0300

    Updated HD

--- MMUProject/src/main/java/com/hit/memoryunits/HardDisk.java
+++ MMUProject/src/main/java/com/hit/memoryunits/HardDisk.java
@@ -1,6 +1,26 @@
 package com.hit.memoryunits;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class HardDisk {
 	
+	private HardDisk() {
+		
+	}
 	
+	public static HardDisk getInstance()
+	{
+		return null;
+	}
+
+	public Page<byte[]> pageFault(java.lang.Long pageId) throws FileNotFoundException, IOException {
+		return null;
+		
+	}
+	
+	public Page<byte[]> pageReplacement(Page<byte[]> moveToHdPage, Long moveToRamId) throws FileNotFoundException, IOException {
+		return moveToHdPage;
+		
+	}
 }
